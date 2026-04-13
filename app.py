f5ba278adc8cab782dc88aed10369620102a1897
Andres <mastro92p@gmail.com>
2026-04-13 19:44:22 +0200

    updated headers

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=ALLOWED_ORIGINS,
     allow_methods=["POST", "GET", "PATCH", "OPTIONS"],
-    allow_headers=["x-api-key", "Content-Type", "x-admin-token", "application/json"],
+    allow_headers=["x-api-key", "Content-Type", "x-admin-token"],
 )
 
 
